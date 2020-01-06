68437f8fe3dff14c5a9f78c3647dbc902e41ebb0
sam@sam <sam@sam>
2020-01-07 02:21:51 +0300

    Elementary samples of Dockerfile

--- docker/somecommands.sh
+++ docker/somecommands.sh
@@ -1,1 +1,3 @@
 docker ps -a | awk '{print $1}' | grep -v CONTAINER | while read -r file; do docker rm $file; done;
+docker images | awk '{print $3}' | grep -v "IMAGE ID" | while read -r file; do docker rmi $file; done;
+cat /proc/net/fib_trie
