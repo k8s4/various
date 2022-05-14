bbc07bf23ff065103347a2d658c0f8ec2ca35585
Diversantos <divsrv@gmail.com>
2022-05-15 00:30:30 +0300

    Added errorhandlers, redirect, abort and shitauth

--- examples/flask/3_flask_contexts_url_for_dynamic_url.py
+++ examples/flask/3_flask_contexts_url_for_dynamic_url.py
@@ -5,13 +5,13 @@
 # app1 = Flask("app1") 
 # app2 = Flask("app2")
 
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "edc8492c-d3c6-11ec-a307-a32b5aa2d1d9"
 
 menu = [{"name": "General", "url": "/"},
-        {"name": "Basepage", "url": "/base"},
+        {"name": "Sign in", "url": "/login"},
         {"name": "Contact", "url": "contact"},
         {"name": "About", "url": "about"}]
 
@@ -37,12 +37,6 @@ def basepage():
     print( url_for("basepage") )
     return render_template("index.html")
 
-# dynamic url eq
-@app.route("/profile/<username>")
-def profile(username):
-    return f"User: {username}\n"
-
-# dynamic url + ...
 # int, float, path
 @app.route("/some/<int:page>/<path:shit>")
 def sometest(shit, page):
@@ -52,7 +46,34 @@ def sometest(shit, page):
 with app.test_request_context():
     print( url_for("basepage") )
     print( url_for("sometest", shit="somesome", page=333) )
-    
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if "userLogged" in session:
+        return redirect(url_for("profile", username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "petrovich" and request.form['password'] == "pass":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Authorize form", menu=menu)
+
+# dynamic url eq
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"User: {username}\n"
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template("404.html", title="Page not found or not yet made", menu=menu), 404
+
+@app.errorhandler(401)
+def pageNotFound(error):
+    return render_template("401.html", title="You are not authorized to view this content", menu=menu, error=401), 401
+
 if __name__ == "__main__":
     app.run(debug=True)
 
