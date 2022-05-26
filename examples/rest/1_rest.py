12b4cc0130aec47bb15d72cdeac8156d35f71e2d
Diversantos <divsrv@gmail.com>
2022-05-26 23:51:07 +0300

    Added little note about RESTful

---
+++ examples/rest/1_rest.py
@@ -0,0 +1,12 @@
+# HTTP methods
+# HEAD, OPTIONS, TRACE, CONNECT(tcp tunnel for encryption)
+# GET(read), POST(write), PUT/PATCH(update, partial update), DELETE(delete)
+
+# Six guidelines for make RESTful API
+# 1. Uniform Interface
+# 2. Client Server Separation
+# 3. Layered System
+# 4. Stateless
+# 5. Cacheable
+# 6. Code on Demand
+
