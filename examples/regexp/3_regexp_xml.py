3c189b62c9477468afa53a0f4a37e1fc2dffc129
Diversantos <divsrv@gmail.com>
2022-05-29 02:18:35 +0300

    Regexp additions

--- examples/regexp/3_regexp_xml.py
+++ examples/regexp/3_regexp_xml.py
@@ -4,7 +4,15 @@ with open("map.xml", "r") as f:
     lat = []
     lon = []
     for data in f:
-        match = re.findall(r"<point\s+[^>]*?lat=([\"\'])([0-9.,]+)\1\s+[^>]*lon=([\"\'])([0-9.,]+)\1", data)
-        print(match)
+#        match = re.findall(r"<point\s+[^>]*?lat=([\"\'])([0-9.,]+)\1\s+[^>]*lon=([\"\'])([0-9.,]+)\1", data)
+#        if len(match) > 0:
+#            lat.append(match[0][1])
+#            lon.append(match[0][3])
+        match = re.search(r"<point\s+[^>]*?lat=([\"\'])(?P<lat>[0-9.,]+)\1\s+[^>]*lon=([\"\'])(?P<lon>[0-9.,]+)\1", data)
+        if match:
+            s = match.groupdict()
+            if "lat" in s and "lon" in s:
+                lat.append(s["lat"])
+                lon.append(s["lon"])
 
     print(lat, lon, sep="\n")
