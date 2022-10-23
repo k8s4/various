7fb9f60b1e5b821fe84903a2e40a4510381a4fd7
Diversantos <divsrv@gmail.com>
2022-10-23 15:26:44 +0300

    Added doc hot to make pypi and deb packages 2

---
+++ examples/hello-world/deb/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="Hello_World",
+        version="0.0.1",
+        author="Pitr",
+        author_email="pitrpitrov@example.shit",
+        url="mysite.shit",
+        description="A hello world exampler shit",
+        packages=find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+)
