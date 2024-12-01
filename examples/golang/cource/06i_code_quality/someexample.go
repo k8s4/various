7cdc9466c362ce68af5bcf7c97e55f465ad3a57d
Diversantos <divsrv@gmail.com>
2024-12-01 14:40:50 +0300

    Some fixes in example after golang liters

--- examples/golang/cource/06i_code_quality/someexample.go
+++ examples/golang/cource/06i_code_quality/someexample.go
@@ -1,53 +1,52 @@
+// Package someexample -.
 package someexample
 
 // Person -.
 type Person struct {
 	Name string
-	Age int
+	Age  int
 }
 
-// GetName blah
+// GetName blah.
 func (p Person) GetName() string {
 	return p.Name
 }
 
+// Avatar struct -.
 type Avatar struct {
-	URL string
+	URL  string
 	Size int64
 }
 
 // Client -.
 type Client struct {
-	ID int64
-	Img Avatar
+	ID   int64
+	Img  Avatar
 	Name string
-	Age int64
+	Age  int64
 }
 
-// HasAvatar ===
-func (c Client) HasAvatar() bool {
-	if c.Img.URL != "" {
-		return true
-	}
-	return false
+// HasAvatar -.
+func (c *Client) HasAvatar() bool {
+	return c.Img.URL != ""
 }
 
-// UpdateAvatar ...
+// UpdateAvatar -.
 func (c *Client) UpdateAvatar() {
 	c.Img.URL = "new_url"
 }
 
 // GetName -.
-func (c Client) GetName() string {
+func (c *Client) GetName() string {
 	return c.Name
 }
 
-//NewClient -.
+// NewClient -.
 func NewClient(name string, age int, img Avatar) *Client {
 	return &Client{
-		ID:	7,
+		ID:   7,
 		Name: name,
-		Age: int64(age),
-		Img: img,
+		Age:  int64(age),
+		Img:  img,
 	}
 }
